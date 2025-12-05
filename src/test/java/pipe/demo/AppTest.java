c32997f01be2c548314bb1c4a1601cb4719cf26e
SL99-zy <ban33114@gmail.com>
2025-12-05 09:43:41 +0100

    pushing apptest

---
+++ src/test/java/pipe/demo/AppTest.java
@@ -0,0 +1,15 @@
+package pipe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
