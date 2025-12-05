2210cc8bd19ae06458b576059b2d485b307ce316
SL99-zy <ban33114@gmail.com>
2025-12-05 09:27:00 +0100

    updated repo structure

--- src/main/java/pipe/demo/DemoApplication.java
+++ src/main/java/pipe/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package java.pipe.demo;
+package pipe.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pipe/demo/DemoApplicationTests.java
+++ src/test/java/pipe/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package java.pipe.demo;
+package pipe.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
